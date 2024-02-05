288a736d8938125faab678770090288b69167cc3
Abhilash Hadagali <117147259+Abhilashhadagali67@users.noreply.github.com>
2024-02-05 23:14:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+// FUnction to create Project card
+
+function project_card(img_src, proj_title, proj_desc, proj_lang1,proj_lang2, proj_lang3,proj_lang4) {
+  let html = `
+    <div class="projects-content">
+      <div class="project-box">
+        <div class="box-project-image">
+          <div class="project-container-image">
+            <img src=${img_src} alt="" />
+          </div>
+        </div>
+        <!-- Image ends -->
+        <div class="box-texts">
+          <h4>${proj_title}</h4>
+          <p>
+          ${proj_desc}
+          </p>
+          <ul>
+            <li>${proj_lang1}</li>
+            <li>${proj_lang2}</li>
+            <li>${proj_lang3}</li>
+            <li>${proj_lang4}</li>
+          </ul>
+          <div class="browser-button">
+            <img src="assets/globe.png" class="globe">
+            <div class="text">Github Repository</div>
+          </div>
+        </div>
+      </div>
+    <!-- project content ends -->
+  </div>`;
+  document.querySelector(".projects-container").innerHTML =
+    document.querySelector(".projects-container").innerHTML + html;
+}
+
+project_card("assets/nick-fewings-MlZnEM4LUwY-unsplash.jpg","car pooling( 2023 )","As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS.","Java","Python");
