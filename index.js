00e38601027bc08f44231e584915e276d51a8d87
Abhilash Hadagali <117147259+Abhilashhadagali67@users.noreply.github.com>
2024-10-06 20:14:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // FUnction to create Project card
 
-function project_card(img_src, proj_title, proj_desc, proj_lang1,proj_lang2, proj_lang3,proj_lang4) {
+function project_card(img_src, proj_title, proj_desc, proj_lang1,proj_lang2, proj_lang3,proj_lang4,github_repo) {
   let html = `
     <div class="projects-content">
       <div class="project-box">
@@ -22,9 +22,11 @@ function project_card(img_src, proj_title, proj_desc, proj_lang1,proj_lang2, pro
             <li>${proj_lang4}</li>
           </ul>
           <div class="browser-button">
-            <img src="assets/globe.png" class="globe">
-            <div class="text">Github Repository</div>
-          </div>
+                <a href=${github_repo} style="display: flex; text-decoration: none;">
+                <img src="assets/globe.png" class="globe" />
+                <div class="text">Github Repository</div>
+              </a>
+              </div>
         </div>
       </div>
     <!-- project content ends -->
@@ -33,14 +35,9 @@ function project_card(img_src, proj_title, proj_desc, proj_lang1,proj_lang2, pro
     document.querySelector(".projects-container").innerHTML + html;
 }
 
-project_card("assets/nick-fewings-MlZnEM4LUwY-unsplash.jpg","car pooling( 2023 )","The car pooling system is a tool which can be used in our day-to-day life, especially in large metropolitian cities.","React","Tailwind","MongoDB","Java");
+// project_card("assets/nick-fewings-MlZnEM4LUwY-unsplash.jpg","car pooling( 2023 )","The car pooling system is a tool which can be used in our day-to-day life, especially in large metropolitian cities.","React","Tailwind","MongoDB","Java");
+project_card("assets/national-cancer-institute-NFvdKIhxYlU-unsplash.jpg","Doctor Appointment( 2024 )","create an Appointment Booking for Doctor or Hospital using React JS","React","MongoDB","Express"," ", "https://github.com/Abhilashhadagali67/real-estate-mern");
 // project_card("assets/nick-fewings-MlZnEM4LUwY-unsplash.jpg","car pooling( 2023 )","As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS.","Java","Python","Django","React");
 // project_card("assets/nick-fewings-MlZnEM4LUwY-unsplash.jpg","car pooling( 2023 )","As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS.","Java","Python","Django","React");
 
 
-function toggleNav() {
-  var navbar = document.getElementById("navbar");
-  navbar.classList.toggle("show");
-  var menuBtn = document.querySelector(".menu-btn");
-  menuBtn.classList.toggle("open");
-}
